ecb79db63a552fac9ceb7efbd95ef32ad916eead
Lucky63 <liceist2008@mail.ru>
2019-10-31 18:09:20 +0600

    update edit method

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,9 +1,9 @@
 export class User {
   constructor(
-    public id: number,
-    public userName: string,
-    public password: string,
-    public lastName:string
+    public id?: number,
+    public userName?: string,
+    public password?: string,
+    public lastName?:string
   ) { }
   
 }
