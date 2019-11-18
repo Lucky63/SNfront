639aeb6009d18dcbf0861a8314e5f6cfe73e38c5
liceist2008@mail.ru <liceist2008@mail.ru>
2019-11-18 17:00:51 +0600

    Added data service to add-friend component

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from '../user';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { DataService } from '../data.service';
 
 
 @Component({
@@ -14,19 +15,15 @@ export class AddFriendComponent implements OnInit {
   user: User;    // изменяемый объект
   loaded: boolean = false;
 
-  constructor(private router: Router, activeRoute: ActivatedRoute, public http: HttpClient) {
+  constructor(private router: Router, activeRoute: ActivatedRoute, public http: HttpClient, private dataService: DataService) {
     this.id = Number.parseInt(activeRoute.snapshot.params["id"]);
   }
 
-  ngOnInit() {    
-    let token = localStorage.getItem("jwt");
-    this.http.get(`http://localhost:5000/api/user/getfriend/${this.id}`,
-      {
-        headers: new HttpHeaders({
-          "Authorization": "Bearer " + token,
-          "Content-Type": "application/json"
-        })
-      }).subscribe(data => this.router.navigateByUrl("/"))  
+  ngOnInit() {
+    
+    this.dataService.getFriend(this.id)
+      .subscribe(data => this.router.navigateByUrl("/"))
+
   }
 
 
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,14 +6,14 @@ import { User } from 'app/user';
 @Injectable()
 export class DataService {
 
-  private token = localStorage.getItem("jwt");
+  private token = localStorage.getItem("jwt");  
 
   private urlCustomers = "http://localhost:5000/api/customers";
-  private urlProduct = "/api/products";
+  private urlUser = "http://localhost:5000/api/user";
   
   constructor(private http: HttpClient) {
   }  
-
+  // CustomersController
   getIdentiUser() {
     return this.http.get(this.urlCustomers + "/" + "getidenti", {
       headers: new HttpHeaders({
@@ -31,5 +31,15 @@ export class DataService {
       })
     });
   }
-  
+
+  //UserController
+
+  getFriend(id:number) {
+    return this.http.get(this.urlUser + "/" + "getfriend" +"/" + id , {
+      headers: new HttpHeaders({
+        "Authorization": "Bearer " + this.token,
+        "Content-Type": "application/json"
+      })
+    });
+  }
 }
